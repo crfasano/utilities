6000dec9afdbf360eb90d949d5691fe8bfae2e98
Cecilia Fasano <cecilia-fasano@uiowa.edu>
2021-08-10 11:19:38 -0400

    setup: Reorganize and create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="utilities",
+    version="0.0.1",
+    author="Casey T. DeRoo, Cecilia R. Fasano, University of Iowa",
+    license="MIT",
+    packages=['utilities'],
+    classifiers=[],
+)
